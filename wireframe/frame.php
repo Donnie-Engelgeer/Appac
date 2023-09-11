322eb74d679e06db76ea262f0d458281773fd282
Joey van Eijk <120835@student.horizoncollege.nl>
2023-09-11 11:54:36 +0200

    Added libraries

--- wireframe/frame.php
+++ wireframe/frame.php
@@ -3,9 +3,19 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
+
+    <nav>
+    </nav>
+
+    <div id="content">
+    </div>
+
+    <div id="footer">
+    </div>
     
 </body>
 </html>
